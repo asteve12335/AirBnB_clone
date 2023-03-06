91f71efbf87898ca4ee798e19aae2dbad37e527f
@steve <asteve12335@gmail.com>
2023-03-07 00:00:37 +0100

    start

---
+++ models/engine/file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module for FileStorage class."""
+import datetime
+import json
+import os
+
+
+class FileStorage:
+
+    """Class for storing and retrieving data"""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        pass
