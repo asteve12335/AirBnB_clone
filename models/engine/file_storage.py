0522db4acd5c2d3f2614fb8faf0f1b4be1786207
@steve <93563603+asteve12335@users.noreply.github.com>
2023-03-10 04:12:30 +0100

    adjustment

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -37,15 +37,33 @@ class FileStorage():
         output = {k: v.to_dict() for k, v in obj_dict.items()}
         with open(FileStorage.__file_path, "w") as f:
             json.dump(output, f, sort_keys=True, indent=4)
+            
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
     def reload(self):
-        """Deserializes the JSON file to __objects."""
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r") as f:
-                obj_dict = json.load(f)
-                obj_dict = {k: self.classes()[v["__class__"]](**v)
+        """Reloads the stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
                         for k, v in obj_dict.items()}
             # TODO: should this overwrite or insert?
             FileStorage.__objects = obj_dict
-        else:
-            pass
