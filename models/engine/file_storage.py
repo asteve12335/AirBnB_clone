c4c7fe0fd83f3d9001a687b7e94bdb6230ed3bee
@steve <93563603+asteve12335@users.noreply.github.com>
2023-03-14 00:37:50 +0100

    file_storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,85 +1,95 @@
 #!/usr/bin/python3
-"""File storage engine module."""
+"""Module for FileStorage class."""
+import datetime
 import json
-from models.base_model import BaseModel
-from models.user import User
-from models.place import Place
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.review import Review
+import os
 
 
 class FileStorage:
-    """Serialize data and save it as json, also deserialize
-    json and create objects.
-    Args:
-        __file_path: string - path to the JSON file (ex: file.json)
-        __objects: dictionary - empty but will store all objects
-        by <class name>.id (ex: to store a BaseModel object with id=12121212,
-        the key will be BaseModel.12121212).
-    """
-    __file_path: str = "file.json"
-    __objects: dict = dict()
+
+    """Class for storing and retrieving data"""
+    __file_path = "file.json"
+    __objects = {}
 
     def all(self):
-        """Returns the dictionary objects <__objects>."""
-
-        return self.__objects
+        """returns the dictionary __objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """ Set in the object in the __object dictionary with the kay
-        which is a combination of the object class and the id
-        example BaseModel.121212
-        obj: instance object to serialize
-        """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serialize and save the object to a json file
-        specified by __file_path
-        """
-        temp_dictionary: dict = {k: v.to_dict() for k, v in
-                                 self.__objects.items()}
+        """ serializes __objects to the JSON file (path: __file_path)"""
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            d = {k: v.to_dict() for k, v in FileStorage.__objects.items()}
+            json.dump(d, f)
 
-        with open(self.__file_path, 'w', encoding='utf-8') as f:
-            json.dump(temp_dictionary, f)
+    def classes(self):
+        """Returns a dictionary of valid classes and their references"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
 
-    def destroy(self, key):
-        """This method deletes instance for the file storage
-        Args:
-            key (str): They of the object instance
-        """
-        try:
-            del self.__objects[key]
-
-            self.save()
-            return True
-        except KeyError:
-            return False
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
 
     def reload(self):
-        """Deserializes the JSON file to __objects
-        (only if the JSON file (__file_path) exists
-        otherwise, do nothing. If the file doesn’t
-        exist, no exception should be raised).
-        """
+        """Reloads the stored objects"""
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+            obj_dict = json.load(f)
+            obj_dict = {k: self.classes()[v["__class__"]](**v)
+                        for k, v in obj_dict.items()}
+            # TODO: should this overwrite or insert?
+            FileStorage.__objects = obj_dict
 
-        __model_classes = {"BaseModel": BaseModel,
-                           "User": User,
-                           "Place": Place,
-                           "State": State,
-                           "City": City,
-                           "Amenity": Amenity,
-                           "Review": Review}
-        temp_dict: dict = {}
-        try:
-            with open(self.__file_path, 'r', encoding='utf-8') as f:
-                temp_dict = json.loads(f.read())
-            for key, value in temp_dict.items():
-                s_key = key.split(".")[0]
-                if s_key in __model_classes.keys():
-                    self.__objects[key] = __model_classes[s_key](**value)
-        except (KeyError, FileNotFoundError):
-            pass
+    def attributes(self):
+        """Returns the valid attributes and their types for classname"""
+        attributes = {
+            "BaseModel":
+                     {"id": str,
+                      "created_at": datetime.datetime,
+                      "updated_at": datetime.datetime},
+            "User":
+                     {"email": str,
+                      "password": str,
+                      "first_name": str,
+                      "last_name": str},
+            "State":
+                     {"name": str},
+            "City":
+                     {"state_id": str,
+                      "name": str},
+            "Amenity":
+                     {"name": str},
+            "Place":
+                     {"city_id": str,
+                      "user_id": str,
+                      "name": str,
+                      "description": str,
+                      "number_rooms": int,
+                      "number_bathrooms": int,
+                      "max_guest": int,
+                      "price_by_night": int,
+                      "latitude": float,
+                      "longitude": float,
+                      "amenity_ids": list},
+            "Review":
+            {"place_id": str,
+                         "user_id": str,
+                         "text": str}
+        }
+        return attributes
